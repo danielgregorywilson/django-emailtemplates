b58be34da3b8fb74d0e0fc9b8c32dd29765b1477
Kyle Rimkus <krimkus@concentricsky.com>
2014-01-31 13:45:54 -0800

    Added settings for DEBUG mode where only whitelisted addresses get sent to

--- emailtemplates/__init__.py
+++ emailtemplates/__init__.py
@@ -1,11 +1,11 @@
-# Copyright 2011 Concentric Sky, Inc. 
-# 
+# Copyright 2011 Concentric Sky, Inc.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,4 +14,4 @@
 
 
 
-VERSION = (0, 10, 2)
+VERSION = (0, 10, 3)
