f02c0c6353a85587b8c30d917aca1d2f0ccee91b
Dan Wilson <dwilson@concentricsky.com>
2014-11-07 12:55:15 -0800

    Update migrations to use AUTH_USER_MODEL instead of auth.User by default. Incompatible with Django < 1.5

--- emailtemplates/migrations/0002_auto__del_requiredcontextitem__del_modeltemplate__add_field_emailtempl.py
+++ emailtemplates/migrations/0002_auto__del_requiredcontextitem__del_modeltemplate__add_field_emailtempl.py
@@ -4,6 +4,18 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+# Safe User import for Django < 1.5
+try:
+    from django.contrib.auth import get_user_model
+except ImportError:
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
+# With the default User model these will be 'auth.User' and 'auth.user'
+# so instead of using orm['auth.User'] we can use orm[user_orm_label]
+user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
+user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
 
 class Migration(SchemaMigration):
 
@@ -30,12 +42,12 @@ class Migration(SchemaMigration):
         db.create_table('emailtemplates_requiredcontextitem', (
             ('template', self.gf('django.db.models.fields.related.ForeignKey')(related_name='required_contexts', to=orm['emailtemplates.ModelTemplate'])),
             ('key', self.gf('django.db.models.fields.SlugField')(max_length=64)),
-            ('updated_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='requiredcontextitem_updated', null=True, to=orm['auth.User'], on_delete=models.SET_NULL, blank=True)),
+            ('updated_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='requiredcontextitem_updated', null=True, to=orm[user_orm_label], on_delete=models.SET_NULL, blank=True)),
             ('type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True, blank=True)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='requiredcontextitem_created', null=True, to=orm['auth.User'], on_delete=models.SET_NULL, blank=True)),
+            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='requiredcontextitem_created', null=True, to=orm[user_orm_label], on_delete=models.SET_NULL, blank=True)),
             ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
         db.send_create_signal('emailtemplates', ['RequiredContextItem'])
@@ -44,12 +56,12 @@ class Migration(SchemaMigration):
         db.create_table('emailtemplates_modeltemplate', (
             ('body', self.gf('django.db.models.fields.TextField')()),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=1024)),
-            ('updated_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='modeltemplate_updated', null=True, to=orm['auth.User'], on_delete=models.SET_NULL, blank=True)),
+            ('updated_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='modeltemplate_updated', null=True, to=orm[user_orm_label], on_delete=models.SET_NULL, blank=True)),
             ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='modeltemplate_created', null=True, to=orm['auth.User'], on_delete=models.SET_NULL, blank=True)),
+            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='modeltemplate_created', null=True, to=orm[user_orm_label], on_delete=models.SET_NULL, blank=True)),
             ('slug', self.gf('django.db.models.fields.CharField')(max_length=255, unique=True, blank=True)),
         ))
         db.send_create_signal('emailtemplates', ['ModelTemplate'])
@@ -75,21 +87,16 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        user_model_label: {
+            'Meta': {
+                'object_name': User.__name__,
+                'db_table': "'%s'" % User._meta.db_table
+            },
+            User._meta.pk.attname: (
+                'django.db.models.fields.AutoField', [],
+                {'primary_key': 'True',
+                'db_column': "'%s'" % User._meta.pk.column}
+            ),
         },
         'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
@@ -103,7 +110,7 @@ class Migration(SchemaMigration):
             'base_template': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'blank': 'True'}),
             'body': ('django.db.models.fields.TextField', [], {'default': "''"}),
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'emailtemplate_created'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': "orm['auth.User']"}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'emailtemplate_created'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': "orm['%s']" % user_orm_label}),
             'from_address': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
@@ -112,7 +119,7 @@ class Migration(SchemaMigration):
             'subject': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
             'txt_body': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'updated_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'emailtemplate_updated'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': "orm['auth.User']"})
+            'updated_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'emailtemplate_updated'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': "orm['%s']" % user_orm_label})
         }
     }
 
